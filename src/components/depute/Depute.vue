ca4bded7bc50c6cc0efcf8fbb778ff923f452611
Julien Gardette <j5492520@saint-gobain.com>
2018-01-17 15:55:24 +0100

    feature: First call to the back

---
+++ src/components/depute/Depute.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="depute">
+    <h1>{{ deputeInfo.prenom }} {{ deputeInfo.nom }}</h1>
+    <h2>description</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'depute',
+  props: {
+    deputeInfo: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
